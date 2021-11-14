9ee742f2a0568bdf2c0a3269ac75de157806d6b0
RCXD <tpxdkss@gmail.com>
2021-11-14 14:39:33 -0600

    Update __init__.py

--- ember/__init__.py
+++ ember/__init__.py
@@ -27,6 +27,7 @@ def vectorize(irow, raw_features_string, X_path, y_path, extractor, nrows):
     """
     Vectorize a single sample of raw features and write to a large numpy file
     """
+    print('testing@@@@@@@@@@@@@@@@@')
     raw_features = json.loads(raw_features_string)
     feature_vector = extractor.process_raw_features(raw_features)
 
